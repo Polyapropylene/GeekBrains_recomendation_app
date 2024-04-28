35cc6c9bece8df41effef9282731c5ea53f97b32
Polyapropylene <92266891+Polyapropylene@users.noreply.github.com>
2024-04-28 06:04:14 +0300

    oh shit finally done card component

---
+++ src/additionalComponents/CourseCard/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import './styles.css';
+
+export type CourseCardProps = {
+	title?: string;
+	salary?: string;
+	description?: string;
+};
+
+export const CourseCard = ({title, salary, description}: CourseCardProps) => {
+	const fire = require('../../assets/fire.svg').default;
+	return (
+	<div className='card'>
+		<div className='upperRow'>
+			<span className='courseType'>Профильный курс</span>
+			<span className='salary'>от {salary !== undefined ? salary : '20 000'} руб</span>
+		</div>
+		<div className='mainText'>
+			<span className="title">{title}</span>
+			<span className="description">{description}</span>
+		</div>
+		<div className='footer'>
+			<div className='leftSide'>
+				<span className="longevity">Срок обучения: 6 месяцев</span>
+				<span className='orangeText'> <img  src={fire} alt='fire' />Главный тренд года</span>
+			</div>
+			<span className='link'>Подробнее</span>
+		</div>
+	</div>)
+};
